455ad7c986bb468fd1b94e362305e6bf081f0f9b
Prabhu <prabh.rao@gmail.com>
2023-03-25 12:20:06 +0530

    roboshop docdb jenkins

--- main.tf
+++ main.tf
@@ -22,10 +22,10 @@ resource "aws_launch_template" "main" {
     )
   }
 
-#  user_data = base64encode(templatefile("${path.module}/userdata.sh", {
-#    component = var.component
-#    env       = var.env
-#  }))
+  user_data = base64encode(templatefile("${path.module}/userdata.sh", {
+    component = var.component
+    env       = var.env
+  }))
 }
 
 resource "aws_autoscaling_group" "main" {
