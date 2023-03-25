919e56b5090942d2ec3957f8022a5e23728bb2d3
Prabhu <prabh.rao@gmail.com>
2023-03-25 19:45:13 +0530

    roboshop docdb jenkins

--- vars.tf
+++ vars.tf
@@ -6,7 +6,7 @@ variable "desired_capacity" {}
 variable "min_size" {}
 variable "max_size" {}
 variable "subnets" {}
-#variable "vpc_id" {}
+variable "vpc_id" {}
 variable "bastion_cidr" {}
 variable "allow_app_to" {}
 variable "port" {}
